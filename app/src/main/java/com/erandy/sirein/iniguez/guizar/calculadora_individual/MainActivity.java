7668c444b515f4de9c52699416b37e8f1aafb0e1
Iñiguez Guizar Erandy Sirein <iniguez.guizar.erandy.sirein@gmail.com>
2021-11-26 00:31:06 -0600

    Primera versión

---
+++ app/src/main/java/com/erandy/sirein/iniguez/guizar/calculadora_individual/MainActivity.java
@@ -0,0 +1,89 @@
+package com.erandy.sirein.iniguez.guizar.calculadora_individual;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonSuma;
+    private Button buttonResta;
+    private Button buttonMultiplicacion;
+    private Button buttonDivision;
+    private Button buttonPromedio;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        buttonSuma = findViewById(R.id.buttonSuma);
+        buttonSuma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirSuma();
+            }
+        });
+        buttonResta = findViewById(R.id.buttonResta);
+        buttonResta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirResta();
+            }
+        });
+        buttonMultiplicacion = findViewById(R.id.buttonMultiplicacion);
+        buttonMultiplicacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirMultiplicacion();
+            }
+        });
+
+        buttonDivision = findViewById(R.id.buttonDivision);
+        buttonDivision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirDivision();
+            }
+        });
+
+        buttonPromedio = findViewById(R.id.buttonPromedio);
+        buttonPromedio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirPromedio();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    private void abrirSuma() {
+        Intent intent = new Intent(this, SumaActivity.class);
+        startActivity(intent);
+    }
+
+    private void abrirResta() {
+        Intent intent = new Intent(this, RestaActivity.class);
+        startActivity(intent);
+    }
+
+    private void abrirMultiplicacion() {
+        Intent intent = new Intent(this, MultiplicacionActivity.class);
+        startActivity(intent);
+    }
+
+    private void abrirDivision() {
+        Intent intent = new Intent(this, DivisionActivity.class);
+        startActivity(intent);
+    }
+    private void abrirPromedio(){
+        Intent intent = new Intent(this, PromedioActivity.class);
+        startActivity( intent );
+    }
+}
---
+++ app/src/main/java/com/erandy/sirein/iniguez/guizar/calculadora_individual/MultiplicacionActivity.java
@@ -0,0 +1,84 @@
+package com.erandy.sirein.iniguez.guizar.calculadora_individual;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MultiplicacionActivity extends AppCompatActivity {
+
+    private EditText editTextX;
+    private EditText editTextY;
+    private EditText editTextTotal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suma);
+        editTextX = findViewById(R.id.editTextNumberX );
+        editTextY = findViewById(R.id.editTextNumberY );
+        editTextTotal = findViewById(R.id.editTextNumberTotal );
+        editTextX.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if( !hasFocus )
+                {
+                    realizaOperacion( );
+                }
+            }
+        });
+        editTextY.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if( !hasFocus )
+                {
+                    realizaOperacion( );
+                }
+            }
+        });
+
+    }
+
+    private void realizaOperacion() {
+        int total = 0;
+        if (editTextX.getText().toString() != null || editTextY.getText().toString() != null)
+            if (isEntero(editTextX.getText().toString())||isEntero(editTextY.getText().toString()))
+                total = (getEntero(editTextX.getText().toString()))*(getEntero(editTextY.getText().toString()));
+            else{}else{}
+        editTextTotal.setText( String.format("%d" , total ) , TextView.BufferType.SPANNABLE );
+        editTextTotal.setEnabled( false );
+    }
+
+    private int getEntero(String entero)
+    {
+        try
+        {
+            return Integer.parseInt( entero );
+        }
+        catch( NumberFormatException ex)
+        {
+            ex.printStackTrace();
+        }
+        return 0;
+    }
+
+    private boolean isEntero(String entero)
+    {
+        try
+        {
+            if( entero == null || entero.length() == 0)
+            {
+                return false;
+            }
+            Integer.parseInt( entero );
+            return true;
+        }
+        catch( NumberFormatException ex)
+        {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+}
